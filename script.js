e6a9c5e9f96e08c4261326a9aae6a482e6412a24
BlairStChair <julia.z15@interia.pl>
2024-07-05 13:36:49 +0200

    Cleaned code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function getComputerChoice(){
     var randomNumber = Math.random() * 4;
     var chosenOptionComputer;
+
     if(randomNumber <= 1.0){
         chosenOptionComputer = "rock";
     }
@@ -19,56 +20,52 @@ function getHumanChoice(){
 }
 
 
-
-function playGame(){
+function playRound(){
     var humanScore = 0;
     var computerScore = 0;
     
 
-       var x = getHumanChoice();
-       var y = getComputerChoice();
+    var x = getHumanChoice();
+    var y = getComputerChoice();
 
-       var h = x.toLowerCase();
-      var c = y.toLowerCase();
-      var result = h.concat(" ", c);
+    var h = x.toLowerCase();
+    var c = y.toLowerCase();
+    var result = h.concat(" ", c);
 
-      console.log("You chose: " + h);
-      console.log("Computer chose: " + c);
+    console.log("You chose: " + h);
+    console.log("Computer chose: " + c);
 
-      if(result == 'rock paper'){
+    if(result == 'rock paper'){
         console.log("You lose! Paper beats Rock");
         computerScore ++;
-      }else if(result == 'rock scissors'){
+    }else if(result == 'rock scissors'){
         console.log("You win! Rock beats Scissors");
-            humanScore ++;
-      }else if(result == 'paper rock'){
+        humanScore ++;
+    }else if(result == 'paper rock'){
         console.log("You win! Paper beats Rock");
-            humanScore ++;
+        humanScore ++;
     }else if(result == 'paper scissors'){
         console.log("You lose! Scissors beats Paper");
-            computerScore ++;
+        computerScore ++;
     }else if(result == 'scissors rock'){
         console.log("You lose! Rock beats Scissors");
-            computerScore ++;
+        computerScore ++;
     }else if(result == 'scissors paper'){
         console.log("You win! Scissors beats Paper");
-            humanScore ++;
+        humanScore ++;
     }else{
         console.log("You get the same option! No one wins!")
     }
 
-console.log("The game is over!\nYour score: " + humanScore + "\nComputer score: " + computerScore);
+    console.log("The game is over!\nYour score: " + humanScore + "\nComputer score: " + computerScore);
 
-if(humanScore > computerScore){
+    if(humanScore > computerScore){
     console.log("You are the winner! c:");
-}else if(humanScore < computerScore){
+    }else if(humanScore < computerScore){
     console.log("You have lost! :c");
-}else if(humanScore == computerScore){
+    }else if(humanScore == computerScore){
     console.log("Draw! No one wins ;-;")
-}
+    }
 }
 
-//  var humanSelection = getHumanChoice();
-var computerSelection = getComputerChoice();
-
-console.log(playGame());
+playRound();
